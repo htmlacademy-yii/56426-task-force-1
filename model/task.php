3c5c0a6ace4e02b5ad0d5c8cd77c0a95e0f8b7bc
Alexey Fetisov <alfet123@gmail.com>
2019-10-14 17:30:15 +0500

    Класс Task. Исправления

--- model/task.php
+++ model/task.php
@@ -3,65 +3,65 @@
 class Task {
 
     public const STATUS_NEW = "Новое задание";
-    public const STATUS_EXECUTE = "Задание выполняется";
-    public const STATUS_FINISH = "Задание завершено";
-    public const STATUS_CANCEL = "Задание отменено";
-    public const STATUS_FAIL = "Задание провалено";
+    public const STATUS_IN_PROGRESS = "Задание выполняется";
+    public const STATUS_FINISHED = "Задание завершено";
+    public const STATUS_CANCELED = "Задание отменено";
+    public const STATUS_FAILED = "Задание провалено";
 
     public const ACTION_ACCEPT = "Принять задание";
     public const ACTION_CANCEL = "Отменить задание";
     public const ACTION_FINISH = "Завершить задание";
-    public const ACTION_REFUSE = "Отказаться от задания";
+    public const ACTION_REJECT = "Отказаться от задания";
 
     public const ROLE_CUSTOMER = "Заказчик";
-    public const ROLE_EXECUTOR = "Исполнитель";
+    public const ROLE_CONTRACTOR = "Исполнитель";
 
-    public $id_customer;
-    public $id_executor;
-    public $completion_time;
-    public $current_status;
+    public $customerId;
+    public $contractorId;
+    public $deadlineTime;
+    public $currentStatus;
 
-    public function __construct($id_customer, $completion_time) {
-        $this->$id_customer = $id_customer;
-        $this->$id_executor = null;
-        $this->$completion_time = $completion_time;
-        $this->$current_status = self::STATUS_NEW;
+    public function __construct($customerId, $deadlineTime) {
+        $this->$customerId = $customerId;
+        $this->$contractorId = null;
+        $this->$deadlineTime = $deadlineTime;
+        $this->$currentStatus = self::STATUS_NEW;
     }
 
     public function getStatusList() {
-        return array(
+        return [
             "STATUS_NEW" => self::STATUS_NEW,
-            "STATUS_EXECUTE" => self::STATUS_EXECUTE,
-            "STATUS_FINISH" => self::STATUS_FINISH,
-            "STATUS_CANCEL" => self::STATUS_CANCEL,
-            "STATUS_FAIL" => self::STATUS_FAIL
-        );
+            "STATUS_IN_PROGRESS" => self::STATUS_IN_PROGRESS,
+            "STATUS_FINISHED" => self::STATUS_FINISHED,
+            "STATUS_CANCELED" => self::STATUS_CANCELED,
+            "STATUS_FAILED" => self::STATUS_FAILED
+        ];
     }
 
     public function getActionList() {
-        return array(
+        return [
             "ACTION_ACCEPT" => self::ACTION_ACCEPT,
             "ACTION_CANCEL" => self::ACTION_CANCEL,
             "ACTION_FINISH" => self::ACTION_FINISH,
-            "ACTION_REFUSE" => self::ACTION_REFUSE
-        );
+            "ACTION_REJECT" => self::ACTION_REJECT
+        ];
     }
 
     public function getStatusNext($action) {
         $nextStatus = null;
-        switch ($this->$current_status) {
+        switch ($this->$currentStatus) {
             case self::STATUS_NEW:
                 if ($action == self::ACTION_ACCEPT) {
-                    $nextStatus = self::STATUS_EXECUTE;
+                    $nextStatus = self::STATUS_IN_PROGRESS;
                 } else if ($action == self::ACTION_CANCEL) {
-                    $nextStatus = self::STATUS_CANCEL;
+                    $nextStatus = self::STATUS_CANCELED;
                 }
                 break;
-            case self::STATUS_EXECUTE:
+            case self::STATUS_IN_PROGRESS:
                 if ($action == self::ACTION_FINISH) {
-                    $nextStatus = self::STATUS_FINISH;
-                } else if ($action == self::ACTION_REFUSE) {
-                    $nextStatus = self::STATUS_FAIL;
+                    $nextStatus = self::STATUS_FINISHED;
+                } else if ($action == self::ACTION_REJECT) {
+                    $nextStatus = self::STATUS_FAILED;
                 }
         }
         return $nextStatus;
