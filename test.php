e88d821fda1c2323b3f71bd49224756c1ad9edc1
Alexey Fetisov <alfet123@gmail.com>
2019-10-21 16:41:39 +0500

    Тестирование через assert

--- test.php
+++ test.php
@@ -1,30 +1,26 @@
 <?php
+require_once('vendor/autoload.php');
+
 use HtmlAcademy\models\Task;
 use HtmlAcademy\models\TaskAction;
 use HtmlAcademy\models\TaskStatus;
 
-require_once('vendor/autoload.php');
-
-$statusList = TaskStatus::getAll();
-
 $myTask = new Task(1);
-var_dump(date("d.m.Y H:i:s", $myTask->createdAt));
-var_dump($statusList[$myTask->currentStatus]);
+
+assert($myTask->currentStatus === TaskStatus::NEW_TASK);
 $myTask->currentStatus = $myTask->getStatusNext(TaskAction::ACCEPT);
-var_dump($statusList[$myTask->currentStatus]);
+assert($myTask->currentStatus === TaskStatus::IN_PROGRESS);
 $myTask->currentStatus = $myTask->getStatusNext(TaskAction::COMPLETE);
-var_dump($statusList[$myTask->currentStatus]);
+assert($myTask->currentStatus === TaskStatus::COMPLETED);
 
 $myTask = new Task(1);
-var_dump(date("d.m.Y H:i:s", $myTask->createdAt));
-var_dump($statusList[$myTask->currentStatus]);
+assert($myTask->currentStatus === TaskStatus::NEW_TASK);
 $myTask->currentStatus = $myTask->getStatusNext(TaskAction::ACCEPT);
-var_dump($statusList[$myTask->currentStatus]);
+assert($myTask->currentStatus === TaskStatus::IN_PROGRESS);
 $myTask->currentStatus = $myTask->getStatusNext(TaskAction::REJECT);
-var_dump($statusList[$myTask->currentStatus]);
+assert($myTask->currentStatus === TaskStatus::FAILED);
 
 $myTask = new Task(1);
-var_dump(date("d.m.Y H:i:s", $myTask->createdAt));
-var_dump($statusList[$myTask->currentStatus]);
+assert($myTask->currentStatus === TaskStatus::NEW_TASK);
 $myTask->currentStatus = $myTask->getStatusNext(TaskAction::CANCEL);
-var_dump($statusList[$myTask->currentStatus]);
+assert($myTask->currentStatus === TaskStatus::CANCELED);
