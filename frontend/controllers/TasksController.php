1e7c6e0f15eeafa23ac7d0e2c530ff6bda3b7b04
Alexey Fetisov <alfet123@gmail.com>
2019-12-12 00:19:13 +0500

    Получение списка задач из БД и их вывод в шаблоне

--- frontend/controllers/TasksController.php
+++ frontend/controllers/TasksController.php
@@ -2,11 +2,13 @@
 namespace frontend\controllers;
 
 use yii\web\Controller;
+use frontend\models\Task;
 
 class TasksController extends Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        $tasks = Task::find()->joinWith('category')->all();
+        return $this->render('index', ['tasks' => $tasks]);
     }
 }
--- frontend/views/tasks/index.php
+++ frontend/views/tasks/index.php
@@ -9,51 +9,19 @@ $this->title = 'Список задач - TaskForce';
 <section class="new-task">
     <div class="new-task__wrapper">
         <h1>Новые задания</h1>
-        <div class="new-task__card">
-            <div class="new-task__title">
-                <a href="#" class="link-regular"><h2>Перевести войну и мир на клингонский</h2></a>
-                <a  class="new-task__type link-regular" href="#"><p>Переводы</p></a>
+        <?php foreach($tasks as $task): ?>
+            <div class="new-task__card">
+                <div class="new-task__title">
+                    <a href="#" class="link-regular"><h2><?= $task->name; ?></h2></a>
+                    <a  class="new-task__type link-regular" href="#"><p><?= $task->category->name; ?></p></a>
+                </div>
+                <div class="new-task__icon new-task__icon--<?= $task->category->icon; ?>"></div>
+                <p class="new-task_description"><?= $task->description; ?></p>
+                <b class="new-task__price new-task__price--<?= $task->category->icon; ?>"><?= $task->budget; ?><b> ₽</b></b>
+                <p class="new-task__place"><?= $task->address; ?></p>
+                <span class="new-task__time"><?= Yii::$app->formatter->asRelativeTime($task->dt_add); ?></span>
             </div>
-            <div class="new-task__icon new-task__icon--translation"></div>
-            <p class="new-task_description">
-                Значимость этих проблем настолько очевидна, что начало
-                повседневной работы по формированию позиции
-                требуют определения и уточнения позиций…
-            </p>
-            <b class="new-task__price new-task__price--translation">3400<b> ₽</b></b>
-            <p class="new-task__place">Санкт-Петербург, Центральный район</p>
-            <span class="new-task__time">4 часа назад</span>
-        </div>
-        <div class="new-task__card">
-            <div class="new-task__title">
-                <a href="#" class="link-regular"><h2>Убраться в квартире после вписки</h2></a>
-                <a class="new-task__type link-regular" href="#"><p>Уборка</p></a>
-            </div>
-            <div class="new-task__icon new-task__icon--clean"></div>
-            <p class="new-task_description">
-                Значимость этих проблем настолько очевидна, что начало
-                повседневной работы по формированию позиции
-                требуют определения и уточнения позиций…
-            </p>
-            <b class="new-task__price new-task__price--clean">1500<b> ₽</b></b>
-            <p class="new-task__place">Санкт-Петербург, Центральный район</p>
-            <span class="new-task__time">5 часов назад</span>
-        </div>
-        <div class="new-task__card">
-            <div class="new-task__title">
-                <a href="#" class="link-regular"><h2>Перевезти груз на новое место</h2></a>
-                <a class="new-task__type link-regular" href="#"><p>Грузоперевозки</p></a>
-            </div>
-            <div class="new-task__icon new-task__icon--cargo"></div>
-            <p class="new-task_description">
-                Значимость этих проблем настолько очевидна, что начало
-                повседневной работы по формированию позиции
-                требуют определения и уточнения позиций…
-            </p>
-            <b class="new-task__price new-task__price--cargo">3000<b> ₽</b></b>
-            <p class="new-task__place">Москва, Центральный район</p>
-            <span class="new-task__time">10 часов назад</span>
-        </div>
+        <?php endforeach; ?>
     </div>
     <div class="new-task__pagination">
         <ul class="new-task__pagination-list">
