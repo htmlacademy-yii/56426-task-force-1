24c1c997f828c1b3c9bb19f749ecf3a237aec1ca
Alexey Fetisov <alfet123@gmail.com>
2020-03-04 18:33:26 +0500

    Создание модели формы и сохранение введенных данных

--- frontend/controllers/SignupController.php
+++ frontend/controllers/SignupController.php
@@ -2,12 +2,44 @@
 
 namespace frontend\controllers;
 
+use Yii;
 use yii\web\Controller;
+use frontend\models\City;
+use frontend\models\User;
+use frontend\models\Profile;
+use frontend\models\UserSignupForm;
 
 class SignupController extends Controller
 {
     public function actionIndex()
     {
-        return $this->render('index', []);
+        $model = new UserSignupForm();
+
+        $cities = City::find()->orderBy(['name' => SORT_ASC])->all();
+
+        $items = ['none' => ''];
+        foreach ($cities as $city) {
+            $items[$city->id] = $city->name;
+        }
+
+        if (Yii::$app->request->getIsPost()) {
+            $formData = Yii::$app->request->post();
+            if ($model->load($formData) && $model->validate()) {
+                $user = new User();
+                $user->email = $model->email;
+                $user->name = $model->name;
+                $user->password = $model->password;
+                if ($user->save()) {
+                    $profile = new Profile();
+                    $profile->user_id = $user->id;
+                    $profile->city_id = $model->city;
+                    if ($profile->save()) {
+                        return Yii::$app->getResponse()->redirect(['tasks']);
+                    }
+                }
+            }
+        }
+
+        return $this->render('index', ['model' => $model, 'items' => $items]);
     }
 }
--- frontend/views/signup/index.php
+++ frontend/views/signup/index.php
@@ -2,6 +2,8 @@
 
 /* @var $this yii\web\View */
 
+use yii\widgets\ActiveForm;
+
 $this->title = 'Регистрация аккаунта - TaskForce';
 
 ?>
@@ -9,26 +11,63 @@ $this->title = 'Регистрация аккаунта - TaskForce';
 <section class="registration__user">
     <h1>Регистрация аккаунта</h1>
     <div class="registration-wrapper">
-        <form class="registration__user-form form-create">
-            <label for="16">Электронная почта</label>
-            <textarea class="input textarea" rows="1" id="16" name="" placeholder="kumarm@mail.ru"></textarea>
+
+        <?php $form = ActiveForm::begin([
+            'id' => 'user-signup-form',
+            'options' => [
+                'class' => 'registration__user-form form-create',
+                'name' => $model->formName()
+            ],
+            'fieldConfig' => [
+                'options' => [
+                    'tag' => false
+                ]
+            ]
+        ]); ?>
+
+            <label for="email"><?=$model->attributeLabels()['email'];?></label>
+            <?php $options = [
+                'class' => 'input textarea',
+                'rows' => '1',
+                'id' => 'email',
+                'tag' => false
+            ]; ?>
+            <?=$form->field($model, 'email', ['template' => "{label}\n{input}"])->input('textarea', $options)->label(false);?>
             <span>Введите валидный адрес электронной почты</span>
-            <label for="17">Ваше имя</label>
-            <textarea class="input textarea" rows="1" id="17" name="" placeholder="Мамедов Кумар"></textarea>
+
+            <label for="name"><?=$model->attributeLabels()['name'];?></label>
+            <?php $options = [
+                'class' => 'input textarea',
+                'rows' => '1',
+                'id' => 'name',
+                'tag' => false
+            ]; ?>
+            <?=$form->field($model, 'name', ['template' => "{label}\n{input}"])->input('textarea', $options)->label(false);?>
             <span>Введите ваше имя и фамилию</span>
-            <label for="18">Город проживания</label>
-            <select id="18" class="multiple-select input town-select registration-town" size="1" name="town[]">
-                <option value="Moscow">Москва</option>
-                <option selected value="SPB">Санкт-Петербург</option>
-                <option value="Krasnodar">Краснодар</option>
-                <option value="Irkutsk">Иркутск</option>
-                <option value="Bladivostok">Владивосток</option>
-            </select>
+
+            <label for="city"><?=$model->attributeLabels()['city'];?></label>
+            <?php $options = [
+                'class' => 'multiple-select input town-select registration-town',
+                'size' => '1',
+                'id' => 'city',
+                'tag' => false
+            ]; ?>
+            <?=$form->field($model, 'city', ['template' => "{label}\n{input}"])->dropDownList($items, $options)->label(false); ?>
             <span>Укажите город, чтобы находить подходящие задачи</span>
-            <label class="input-danger" for="19">Пароль</label>
-            <input class="input textarea " type="password" id="19" name="">
+
+            <label class="input-danger" for="password"><?=$model->attributeLabels()['password'];?></label>
+            <?php $options = [
+                'class' => 'input textarea',
+                'rows' => '1',
+                'id' => 'password',
+                'tag' => false
+            ]; ?>
+            <?=$form->field($model, 'password', ['template' => "{label}\n{input}"])->input('password', $options)->label(false);?>
             <span>Длина пароля от 8 символов</span>
+
             <button class="button button__registration" type="submit">Cоздать аккаунт</button>
-        </form>
+
+        <?php ActiveForm::end(); ?>
+
     </div>
 </section>
